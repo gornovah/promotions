57712fd1eec74576026688bf673ce852f879d7b7
Domingo <domingo.espinosa@opentrends.net>
2019-09-16 11:37:02 +0200

    first commit

---
+++ src/test/java/com/architecture/promotions/PromotionsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.architecture.promotions;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PromotionsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
